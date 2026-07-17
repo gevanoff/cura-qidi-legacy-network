0202b6ceb5cd915824f520c3477a55b982b2520c
gevanoff <gevanoff@gmail.com>
2026-07-16 22:49:33 -0700

    Test connected UDP endpoint selection

--- tests/test_transport.py
+++ tests/test_transport.py
@@ -1,10 +1,13 @@
 from qidi_legacy.transport import UdpTransport
 
 
-def test_udp_transport_binds_ephemeral_port_before_receive() -> None:
+def test_udp_transport_connects_ephemeral_port_before_receive() -> None:
     with UdpTransport("127.0.0.1") as transport:
-        local_host, local_port = transport._socket.getsockname()
+        local_host, local_port = transport.local_endpoint
+        remote_host, remote_port = transport._socket.getpeername()
 
-        assert local_host in {"0.0.0.0", "127.0.0.1"}
+        assert local_host == "127.0.0.1"
         assert local_port > 0
+        assert remote_host == "127.0.0.1"
+        assert remote_port == 3000
         transport.discard_pending()
